d74c255a3e555cdb589727ae76b94ce8a164efab
Leske <leske@mensa.hu>
2025-07-10 12:47:52 +0200

    refactor: change RescourceChecker class

--- src/stage3/ResourceChecker.java
+++ src/stage3/ResourceChecker.java
@@ -3,11 +3,21 @@ package stage3;
 import stage2.ResourceCalculator;
 
 public class ResourceChecker {
-    public ResourceChecker(Resources needed, Resources available) {
+    private final float water_available;
+    private final float food_available;
+    private final int   tanks_available;
+
+    public ResourceChecker(Resources available) {
+        water_available = available.getWater();
+        food_available = available.getFood();
+        tanks_available = available.getTanks();
+    }
+
+    public ResourceCheckResult checkResources(Resources needed) {
         int people = needed.getPeople();
-        int days_water = (int) (available.getWater() / (needed.getWater() * people));
-        int days_food = (int) (available.getFood() / (needed.getFood() * people));
-        int days_tanks = available.getTanks() / (needed.getTanks() * people);
+        int days_water = (int) (water_available / (needed.getWater() * people));
+        int days_food = (int) (food_available / (needed.getFood() * people));
+        int days_tanks = tanks_available / (needed.getTanks() * people);
 
         int days_min = Math.min(days_water, Math.min(days_food, days_tanks));
         boolean journey = (days_min >= needed.getDays());
@@ -15,6 +25,6 @@ public class ResourceChecker {
         int plus_days = -1;
         if (plus)
             plus_days = days_min - needed.getDays();
-        ResourceCheckResult res = new ResourceCheckResult(journey, plus, plus_days, days_min);
+        return new ResourceCheckResult(journey, plus, plus_days, days_min);
     }
 }
