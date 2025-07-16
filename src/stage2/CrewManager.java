86fcb0e23accf9568c3c6b9be04baa4921476541
Leske <leske@mensa.hu>
2025-07-16 13:05:23 +0200

    feat: implement CrewManager class

---
+++ src/stage2/CrewManager.java
@@ -0,0 +1,32 @@
+package stage2;
+
+import java.lang.reflect.Array;
+
+public class CrewManager {
+    private ICrewMember[] crew = new ICrewMember[10];
+    private int crewSize = 0;
+
+    public CrewManager() {
+        addCrewMember(new Captain("Morgan", 100));
+        addCrewMember(new CombatPirate("Bob Wanderer", 100));
+        addCrewMember(new CombatPirate("Jane Sword", 100));
+    }
+
+    public void addCrewMember(ICrewMember member) {
+        if (crewSize < crew.length) {
+            crew[crewSize] = member;
+            crewSize++;
+        } else {
+            System.out.println("Crew is full");
+        }
+    }
+
+    public void viewCrew() {
+        System.out.println("\n[Crew Information]");
+        if (crewSize == 0)
+            System.out.println("Crew is empty"); //tho this will likely never happen
+        for (int i = 0; i < crewSize; i++) {
+            System.out.println(crew[i].getInfo());
+        }
+    }
+}
