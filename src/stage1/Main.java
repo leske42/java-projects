3d01f4a33440f1581f234e9038eabb0cf66a25c8
Leske <leske@mensa.hu>
2025-07-16 14:08:04 +0200

    refactor: implement adding crew members in main

--- src/stage1/Main.java
+++ src/stage1/Main.java
@@ -1,17 +1,23 @@
 package stage1;
 
+import stage2.Captain;
+import stage2.CombatPirate;
 import stage2.CrewManager;
+import stage2.ICrewMember;
 
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
+    private static final int CAPTAIN_COST = 200;
+    private static final int COMBAT_PIRATE_COST = 100;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         CrewManager crewManager = new CrewManager();
         System.out.println("Welcome, Captain! Prepare for adventure in the galaxy.");
-        int totalPoints = 0;
+        int totalPoints = 1000;
         while (true) {
             System.out.println("\nMain Menu:");
             System.out.println("1 - View Crew\n2 - Launch a raid");
@@ -30,7 +36,7 @@ public class Main {
                     repairSystems();
                     break;
                 case 4:
-                    addCrewMember();
+                    totalPoints -= addCrewMember(sc, totalPoints, crewManager);
                     break;
                 case 5:
                     statistics();
@@ -63,11 +69,6 @@ public class Main {
         }
     }
 
-    private static void viewCrew() {
-        System.out.println("\n[Crew Information]");
-        System.out.println("Crew is currently empty");
-    }
-
     private static void launchRaid(Scanner sc) {
         System.out.println("\nSelect Raid Target:");
         System.out.println("1. Civilian Ship\n2. Military Ship\n3. Unknown Vessel");
@@ -95,9 +96,40 @@ public class Main {
         System.out.println("Systems are 100% healthy");
     }
 
-    private static void addCrewMember() {
+    private static int addCrewMember(Scanner sc, int points, CrewManager crew) {
+        sc.nextLine(); //discard newline from number choice here
         System.out.println("\n[Add Crew Member]");
-        System.out.println("Not enough money");
+        System.out.print("Enter name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter role: ");
+        String role = sc.nextLine().toLowerCase();
+        if (!role.equals("captain") && !role.equals("combat pirate")) {
+            System.out.println("Invalid role. Crew member not added.");
+            return 0;
+        }
+        int deduct = 0;
+        ICrewMember member;
+        if (role.equals("captain")) {
+            if (points < CAPTAIN_COST) {
+                System.out.println("Not enough points to add a Captain");
+                return 0;
+            }
+            member = new Captain(name, 100);
+            deduct = CAPTAIN_COST;
+        }
+        else {
+            if (points < COMBAT_PIRATE_COST) {
+                System.out.println("Not enough points to add a Combat Pirate");
+                return 0;
+            }
+            member = new CombatPirate(name, 100);
+            deduct = COMBAT_PIRATE_COST;
+
+        }
+        crew.addCrewMember(member);
+        System.out.println("New crew member added: " + member.getInfo());
+        System.out.println("Points deducted: " + deduct);
+        return deduct;
     }
 
     private static void statistics() {
